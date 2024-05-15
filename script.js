98bb6ea5008ef33c766ced3f390b0d23479311d7
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-05-15 22:00:45 +1000

    Factory function

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 // Object
 
-const person = {
-  name: "Thakur ",
-  add: "Sysdney",
-  bio() {
-    return `
-        Hey this is ${this.name} , live in ${this.add}`;
-  },
-};
-console.log(person.bio());
+// const person = {
+//   name: "Thakur ",
+//   add: "Sysdney",
+//   bio() {
+//     return `
+//         Hey this is ${this.name} , live in ${this.add}`;
+//   },
+// };
+// console.log(person.bio());
+
+// FACTORY FUNCTION
+function aboutPerson(name, add, phone = "N / A") {
+  return {
+    name,
+    add,
+    phone,
+    bio() {
+      return `
+                  Hey this is ${this.name} , live in ${this.add} and my phone is ${this.phone}`;
+    },
+  };
+}
+const ThakurObj = aboutPerson("Thakur", "Sydney", "0987654321");
+console.log(ThakurObj.bio());
+const samObj = aboutPerson("Sam", "Melbourne");
+console.log(samObj, samObj.bio());
